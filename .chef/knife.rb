cea36e91b9cd11c62f8c6c6ababea9f22a317d94
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-02-10 21:03:05 -0800

    Add Knife configuration.

---
+++ .chef/knife.rb
@@ -0,0 +1,6 @@
+log_level       :info
+log_location    STDOUT
+cookbook_path   [
+                  File.expand_path("../../site-cookbooks", __FILE__),
+                  File.expand_path("../../cookbooks", __FILE__),
+                ]
